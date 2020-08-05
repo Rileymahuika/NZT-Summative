abf7c043a1c087064ac84d893c48c79a1844adbe
riley.mahuika <riley.mahuika@gmail.com>
2020-08-05 23:32:00 +1200

    Needs debugging

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,224 +1,216 @@
-const el_regionList = document.querySelector("#region-dropdown");
-const el_islandList = document.querySelector("#island-dropdown");
-const el_accommodationList = document.querySelector("#accommodation-dropdown");
-const el_roomListings = document.querySelector("#room-showcase");
-const el_menuNav = document.querySelector("#menu-nav");
-const el_stayingList = document.querySelector("#nights__dropdown");
-
-// var displayRegions = [];
-// var displayIslands = [];
+const el_regionList = $("#region-dropdown");
+const el_islandList = $("#island-dropdown");
+const el_accommodationList = $("#accommodation-dropdown");
+const el_roomListings = $("#room-showcase");
+const el_menuNav = $("#menu-nav");
+const el_minStayingList = $("#nights__dropdown1");
+const el_maxStayingList = $("#nights__dropdown2");
+const el_detailedRooms = $("#rooms-filter");
+const el_categoryList = $("#accommodation-dropdown")
+var categoryArray = [];
 
 // JSON STUFF //////////////////////////////////
 function init() {
-  $.getJSON("json/regions.json", function (data) {
-    regions = data.regions;
-    console.log(regions);
-    displayRegions(regions);
-  });
 
-  $.getJSON('json/type.json', function (data) {
-    typeArray = data.type;
-    console.log(typeArray);
-    displayType(typeArray);
+  $.getJSON("json/type.json", function (data) {
+    let categoryArray = data.type;
+    console.log(categoryArray);
+    displayCategories(categoryArray)
   });
 
   $.getJSON("json/islands.json", function (data) {
-    islands = data.islands;
-    console.log(islands);
+    let islands = data.islands;
     displayIslands(islands);
   });
 
   $.getJSON("json/accommodation.json", function (data) {
-    accommodation = data.accommodation;
-    rooms = data.accommodation;
-    stays = data.accommodation;
-    displayAccommodation(accommodation);
+    let rooms = data.accommodation;
+    let stays = data.accommodation;
+    let details = data.accommodation;
     displayRooms(rooms);
-    displayStay(stays);
+    displayDetailedRooms(details);
+    displayMinStay(stays);
+    displayMaxStay(stays);
   });
-
 }
-
-// NAV ///////////////////////////////////////////
-
-function displayNav(pages) {
-  let html = "";
-  for (var i = 0; i < pages.length; i++) {
-    console.log(i + pages[i]);
-  }
-  el_menuNav.innerHTML = html;
-
-  return `
-<li id="menu-nav__item">
-<a id="menu-nav__link" href="/dist/mainpage.html">${pages[i]}</a>
-</li>
-  `;
-}
-
-var pages = ["Home", "Destinations", "Things to do", "About", "STAY"];
 
 
 // ISLANDS //////////////////////////////////
-
 // display list of islands
 function displayIslands(islands) {
   let html = "";
   for (island of islands) {
     html += islandHTML(island);
   }
-  el_islandList.innerHTML = html;
+  el_islandList.html(html);
+
+  el_islandList.change(function () {
+    $.getJSON("json/regions.json", function (data) {
+      let regions = data.regions;
+      let islandId = el_islandList.val();
+      let regionIslandFilter = filterRegionsByIsland(regions, islandId);
+      displayRegions(regionIslandFilter);
+    });
+  });
 }
 
 // return list
 function islandHTML(island) {
   return `
-<option id="island-dropdown__extended" value="North">${island.name}</option>
+<option id="island-dropdown__extended" value="${island.id}">${island.name}</option>
     `;
 }
 
-
+function filterRegionsByIsland(regions, island) {
+  let matchings = [];
+  for (region of regions) {
+    if (region.id == island) {
+      matchings.push(region);
+    }
+  }
+  return matchings;
+};
 // REGIONS //////////////////////////////////
-
 // display list of regions
 function displayRegions(regions) {
   let html = "";
   for (region of regions) {
-    html += northHTML(region);
-  }
-  for (region of regions) {
-    html += southHTML(region);
+    html += regionHTML(region);
   }
   el_regionList.innerHTML = html;
 }
 
-// return list
-function southHTML(region) {
-  return `
-<option id="region-dropdown" value="North">${region.name}</option>
-    `;
-}
-
 // list of returned from selected island
-
-function northHTML(region) {
+function regionHTML(region) {
   return `
-<option id="region-dropdown" value="North">${region.name}</option>
+<option id="region-dropdown" value="${region.id}">${region.name}</option>
     `;
-    
-}
-
-// ACCOMMODATION /////////////////////////////////
-
-// display accommodation type
-function displayAccommodation(accommodation) {
-  let html = "";
-  for (accommo of accommodation) {
-    html += accomoHTML(accommo);
-  }
-  el_accommodationList.innerHTML = html;
-
-  addCategoryClickListener();
-}
-
-// return accommodation type list
-function accomoHTML(accommo) {
-  return `
-<option id="accommodation-selector" data-id="${accommo.id}" value="Accommodation">${accommo.type}</option>
-    `;
-}
-// SORT ACCOMODATION BY CATEGORY
-function displayType(type) {
-  var type = [];
-  var typeArray = [];
-  for (var i = 0; i < typeArray.length; i++) {
-    if (typeArray[i].typeArray === type) {
-      matches.push(typeArray[i]);
-    }
-  }
-  displayType(type);
-}
-
-function addCategoryClickListener() {
-  $('#accommodation-selector').click(function () {
-    var id = $(this).data('id');
-    displayAccommodation(id);
-  });
 }
 
 // SEARCH BUTTON //////////////////////////////////
-
-// make search button toggle 
-$('input').submit(function (event) {
-  event.preventDefault();
-
-  validateForm($(this)[0]);
+// make search button toggle
+$("button").click(function() {
+  $("wrapper").show( "fast");
 });
 
-function validateForm(form) {
-  let isError = false;
-
-  const selects = form.elements;
+// ACCOMMODATION /////////////////////////////////
+// VIEW CATEGORY /////////////////////////////
+function displayCategories(categories) {
+  let html = "";
+  for (let i = 0; i < categories.length; i++ ) {
+    // console.log(categories[i].title);
+    html += makeCategoryHtml(categories[i]);
+  }
+  el_categoryList.html(html)
+  addCategoryClickListener();
 }
 
-//  CALLENDER //////////////////////////////////
+function makeCategoryHtml(category){
+  return `
+  <li class="accommodation-selector" data-id="${category.id}">${category.title}</li>
+  `
+}
 
-// might just use basic option selector
+// CATEGORY FILTER /////////////////////////////////
+function getAccommodationByCategory() {
+  let matches = [];
+  for (let i = 0; i < categoryArray.length; i++) {
+    if (categoryArray[i].categoryId) {
+      matches.push(categoryArray[i]);
+    };
+  }
+  console.log(matches)
+  displayRooms(matches)
+}
 
-// TOTAL NIGHTS STAYING /////////////////////////////////////////
+// CATEGORY CLICK LISTENER /////////////////////////
+function addCategoryClickListener() {
+  $('.accommodation-selector').click(function() {
+    let id = $(this).data('id');
+    getAccommodationByCategory();
+  })
+}
 
-function displayStay(stays) {
+// FILTER BY GUESTS //////////////////////////////////////////////
+
+// MIN NIGHTS STAYING /////////////////////////////////////////
+function displayMinStay(stays) {
   let html = "";
   for (stay of stays) {
-    html += totalNightsHTML(stay);
+    html += minNightsHTML(stay);
   }
-  el_stayingList.innerHTML = html;
+  el_minStayingList.innerHTML = html;
 }
 
 // return list
-function totalNightsHTML(stay) {
+function minNightsHTML(stay) {
   return `
 <option id="nights__selector" type="button" name="min">${stay.minStay}</option>
     `;
 }
-
-// ROOMS //////////////////////////////////
-function displayRooms(rooms) {
+// MAX NIGHTS STAYING /////////////////////////////////////////
+function displayMaxStay(stays) {
   let html = "";
-  for (room of rooms) {
-    html += makeHTML(room);
-    html += makeHTML2(room);
+  for (stay of stays) {
+    html += maxNightsHTML(stay);
   }
-  el_roomListings.innerHTML = html;
+  el_maxStayingList.innerHTML = html;
+  console.log(stay)
 }
 
+// return list
+function maxNightsHTML(stay) {
+  return `
+<option id="nights__selector" type="button" name="max">${stay.maxStay}</option>
+    `;
+}
 
-function makeHTML(room) {
+// ROOMS //////////////////////////////////
+// display rooms
+function displayRooms(rooms) {
+  var html = '';
+  for (var i = 0; i < rooms.length; i++) {
+    html += makeRoomHTML(rooms[i]);
+  }
+el_roomListings.html(html);
+}
+
+function makeRoomHTML(roomObjects) {
   return `
   <div id="room-showcase__room">
     <div id="room-showcase__img">
       <img id="room-showcase__img-img" src="https://picsum.photos/300/200" alt="">
     </div>
     <div id="room-showcase__info">
-      <h1 id="room-showcase__roomHeader">${room.type}</h1>
-      <p id="room-showcase__roomInfo">${room.text}</p>
-      <p id="room-showcase__roomInfo">Min Stay: ${room.minStay}</p>
-      <p id="room-showcase__roomInfo">Max Stay: ${room.maxStay}</p>
-      <p id="room-showcase__roomInfo">Total Guests: ${room.guests}</p>
+      <h1 id="room-showcase__roomHeader">${roomObjects.type}</h1>
+      <p id="room-showcase__roomInfo">${roomObjects.text}</p>
+      <p id="room-showcase__roomInfo">Min Stay: ${roomObjects.minStay}</p>
+      <p id="room-showcase__roomInfo">Max Stay: ${roomObjects.maxStay}</p>
+      <p id="room-showcase__roomInfo">Total Guests: ${roomObjects.guests}</p>
     </div>  
   </div>
 
   <div id="room-showcase__select">
-    <h4 id="room-showcase__select-button" onclick="myFunction()" >Expand Options</h4>
+  <h4 id="room-showcase__select-button">Expand Options</h4>
   </div>
     `;
 }
 
+function displayDetailedRooms(details) {
+  let html = "";
+  for (detail of details) {
+    html += makeHTML2(detail);
+  }
+  el_detailedRooms.innerHTML = html;
+}
+
+$("h4").click(function() {
+  $("main").show( "slow");
+});
+
 function makeHTML2(room) {
-
   // on click function to toggle when button is clicked
-
-
-  return `      <div class="rooms-filter">
+  return `
   <img
     class="rooms-filter__image"
     src="https://picsum.photos/300/200"
@@ -263,17 +255,12 @@ function makeHTML2(room) {
 <div class="menu">
 <h3 class="menu__menu">MENU</h3>
 </div>
-</div>`
+</div>`;
 }
-
-// FILTER BY GUESTS //////////////////////////////////////////////
 
 // filter adults by amount of guests required
 
-
 // SELECT MENU OPTIONS ///////////////////////////////////////////
-
-
 
 // RUN-------------------------
 init();
